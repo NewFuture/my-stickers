3f47250a3b8cc5c9ba6c70ca68589c5544e6d556
New Future (Weilai LIU) <New.Future@microsoft.com>
2019-11-20 20:45:37 +0800

    feat(server): 分页+模糊搜索

--- server/src/teams/query.ts
+++ server/src/teams/query.ts
@@ -1,7 +1,7 @@
 import * as debug from "debug";
-import { CardFactory, MessagingExtensionQuery, MessagingExtensionResult, MessagingExtensionAttachment, MessagingExtensionSuggestedAction } from "botbuilder";
+import { CardFactory, MessagingExtensionQuery, MessagingExtensionResult, MessagingExtensionAttachment, MessagingExtensionSuggestedAction, MessagingExtensionParameter, MessagingExtensionQueryOptions } from "botbuilder";
 import { Request } from "express";
-import { Sticker, getUserStickers } from "../services/sticker";
+import { Sticker, getUserStickers, searchUserStickers } from "../services/sticker";
 import { getConfigUrl } from "./getConfigUrl";
 import { Locale } from "../config";
 
@@ -29,6 +29,47 @@ function stickerToCard(sticker: Sticker): MessagingExtensionAttachment {
     return card;
 }
 
+function getConfig(req: Request): MessagingExtensionResult {
+    return {
+        type: "config",
+        text: req.__(Locale.initial_run_upload_stickers),
+        suggestedActions: {
+            actions: [{
+                type: "openUrl",
+                title: req.__(Locale.initial_run_upload_stickers),
+                // image: "https://cataas.com/cat/gif",
+                // text: "UPLOAD",
+                // displayText: "上传",
+                value: getConfigUrl(req.userId)
+            }]
+        } as MessagingExtensionSuggestedAction,
+    };
+}
+
+/**
+ * 分页
+ * @param data 
+ * @param options 
+ */
+function pagination(data: Sticker[], options?: MessagingExtensionQueryOptions): MessagingExtensionResult {
+    const stickers = options ? data.slice(options.skip || 0, options.count && ((options.skip || 0) + options.count)) : data;
+    return {
+        type: "result",
+        attachmentLayout: "grid",
+        attachments: stickers.map(stickerToCard)
+    }
+}
+
+/**
+ * 获取查询参数
+ * @param name 
+ * @param params 
+ */
+function getQueryParameters(name: string, params?: MessagingExtensionParameter[]) {
+    return params && params.find(p => p.name === name)?.value;
+}
+
+
 /**
  * 表情选择款弹出或者搜索
  * @param context
@@ -37,27 +78,16 @@ function stickerToCard(sticker: Sticker): MessagingExtensionAttachment {
 export async function queryMyCollection(req: Request, query: MessagingExtensionQuery): Promise<MessagingExtensionResult> {
     const id = req.userId;
     log("onQuery", id, query);
-    try {
+    const search = getQueryParameters("query", query.parameters);
+    if (search) {
+        const stickers = await searchUserStickers(id, search);
+        return pagination(stickers, query.queryOptions);
+    } else {
         const stickers = await getUserStickers(id);
-        return {
-            type: "result",
-            attachmentLayout: "grid",
-            attachments: stickers.map(stickerToCard)
-        };
-    } catch (error) {
-        return {
-            type: "config",
-            text: req.__(Locale.initial_run_upload_stickers),
-            suggestedActions: {
-                actions: [{
-                    type: "openUrl",
-                    title: req.__(Locale.initial_run_upload_stickers),
-                    // image: "https://cataas.com/cat/gif",
-                    // text: "UPLOAD",
-                    // displayText: "上传",
-                    value: getConfigUrl(id)
-                }]
-            } as MessagingExtensionSuggestedAction,
-        };
+        if (stickers && stickers.length) {
+            return pagination(stickers, query.queryOptions);
+        } else {
+            return getConfig(req);
+        }
     }
 }
