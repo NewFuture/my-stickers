386114eb6a07b7eefa136ca493c99113ea590b83
NewFuture <New.Future@microsoft.com>
2019-11-23 18:35:46 +0800

    fix(web): force render

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root")!;
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
+// if (rootElement.hasChildNodes()) {
+//     hydrate(<App />, rootElement);
+// } else {
+//     render(<App />, rootElement);
+// }
+render(<App />, rootElement);
+
