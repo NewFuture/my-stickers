40ef80ee9a46da5ca31ef1470066a79955436ae4
New Future (Weilai LIU) <New.Future@microsoft.com>
2020-01-02 14:19:47 +0800

    feat(taskmodal): update size

--- server/src/teams/fetchTask.ts
+++ server/src/teams/fetchTask.ts
@@ -1,5 +1,13 @@
 import * as debug from "debug";
-import { CardFactory, CardImage, MessagingExtensionAttachment, TaskModuleResponse, MessagingExtensionAction, MessageActionsPayloadAttachment, TaskModuleContinueResponse } from "botbuilder";
+import {
+    CardFactory,
+    CardImage,
+    MessagingExtensionAttachment,
+    TaskModuleResponse,
+    MessagingExtensionAction,
+    MessageActionsPayloadAttachment,
+    TaskModuleContinueResponse,
+} from "botbuilder";
 import { Request } from "express";
 import { addUserStickers } from "../services/sticker";
 import { Locale } from "../config/";
@@ -9,7 +17,10 @@ const log = debug("collect");
 
 const ImgSrcRegex = /<img[^>]+src="([^"\s]+)"[^>]*>/g;
 const ImgAltRegex = /<img[^>]+alt="([^"]+)"[^>]*>/;
-interface Img { src: string; alt?: string | null; }
+interface Img {
+    src: string;
+    alt?: string | null;
+}
 // 字符串中提取图片信息
 function getImages(text: string): Img[] {
     const urls: Img[] = [];
@@ -29,9 +40,12 @@ function getImageFromAttachment(a: MessagingExtensionAttachment): Img[] {
         // download attach
         return [{ src: a.contentUrl! }];
     }
-    if (a.contentType === "application/vnd.microsoft.card.adaptive" || a.contentType === "application/vnd.microsoft.card.hero") {
-        const card: any = typeof a.content === "string" ? JSON.parse(a.content || "{\"body\":[]}") : a.content;
-        return card.body.filter(c => c.type === "Image").map((c) => ({ src: c.url, alt: c.alt || c.altText }));
+    if (
+        a.contentType === "application/vnd.microsoft.card.adaptive" ||
+        a.contentType === "application/vnd.microsoft.card.hero"
+    ) {
+        const card: any = typeof a.content === "string" ? JSON.parse(a.content || '{"body":[]}') : a.content;
+        return card.body.filter(c => c.type === "Image").map(c => ({ src: c.url, alt: c.alt || c.altText }));
     }
     return [];
 }
@@ -42,61 +56,71 @@ export async function fetchTaskCollect(req: Request, value: MessagingExtensionAc
     const payload = value.messagePayload || {};
     const imgs = getImages(payload.body!.content!);
     const attachments: MessageActionsPayloadAttachment[] = payload.attachments || [];
-    attachments.map(getImageFromAttachment).filter(s => !!s).forEach(s => imgs.push(...s));
+    attachments
+        .map(getImageFromAttachment)
+        .filter(s => !!s)
+        .forEach(s => imgs.push(...s));
 
     log("imgs", imgs);
     const hasImgs = imgs && imgs.length > 0;
 
-    const saveImgs = hasImgs && await addUserStickers(id, imgs.map(img => ({ src: img.src, name: img.alt! })));
+    const saveImgs =
+        hasImgs &&
+        (await addUserStickers(
+            id,
+            imgs.map(img => ({ src: img.src, name: img.alt! })),
+        ));
     // log('imgs', saveImgs)
 
-
-    const card = CardFactory.adaptiveCard(
-        {
-            type: "AdaptiveCard",
-            $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
-            version: "1.0",
-            body: saveImgs ? [
-                ...saveImgs.map<CardImage>(img => ({
-                    url: img.src,
-                    type: "Image",
-                    altText: img.name,
-                    spacing: "None",
-                }))
-            ] : [
-                    {
-                        type: "TextBlock",
-                        text: req.__(Locale.collect_save_no_images_found),
-                        horizontalAlignment: "Center",
-                        wrap: true,
-                    }
-                ],
-            // actions: [
-            //     // {
-            //     //     type: "Action.Submit",
-            //     //     title: "删除",
-            //     //     data: {
-            //     //         action: "moreDetails",
-            //     //         id: "1234-5678"
-            //     //     }
-            //     // },
-            //     // {
-            //     //     type: "Action.Submit",
-            //     //     title: "确定",
-            //     //     data: {
-            //     //         id: "1234-5678"
-            //     //     }
-            //     // },
-            // ],
-        });
+    const card = CardFactory.adaptiveCard({
+        type: "AdaptiveCard",
+        $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+        version: "1.0",
+        body: saveImgs
+            ? [
+                  ...saveImgs.map<CardImage>(img => ({
+                      url: img.src,
+                      type: "Image",
+                      altText: img.name,
+                      spacing: "None",
+                      width: 200,
+                      height: 200,
+                  })),
+              ]
+            : [
+                  {
+                      type: "TextBlock",
+                      text: req.__(Locale.collect_save_no_images_found),
+                      horizontalAlignment: "Center",
+                      wrap: true,
+                  },
+              ],
+        // actions: [
+        //     // {
+        //     //     type: "Action.Submit",
+        //     //     title: "删除",
+        //     //     data: {
+        //     //         action: "moreDetails",
+        //     //         id: "1234-5678"
+        //     //     }
+        //     // },
+        //     // {
+        //     //     type: "Action.Submit",
+        //     //     title: "确定",
+        //     //     data: {
+        //     //         id: "1234-5678"
+        //     //     }
+        //     // },
+        // ],
+    });
     const result: TaskModuleContinueResponse = {
         type: "continue",
         value: {
             title: req.__(saveImgs ? Locale.collect_save_success : Locale.collect_save_fail),
-            height: "small",
-            width: "small",
+            height: saveImgs ? 300 : 40,
+            width: saveImgs ? 300 : "small",
             card,
-        }
+        },
     };
     return Promise.resolve({
         task: result,
