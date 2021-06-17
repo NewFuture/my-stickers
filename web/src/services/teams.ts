88b39cbdb12cdf89ae382f128dd4a78b8f2c1ed8
NewFuture <New.Future@microsoft.com>
2021-06-17 12:42:16 +0800

    init

--- web/src/services/teams.ts
+++ web/src/services/teams.ts
@@ -5,29 +5,33 @@ export const auth = {
     token: '',
 }
 
+teams.initialize(() => {console.debug("teams initialized")})
+
 export function init() {
-    return new Promise((resolve, reject) => {
-        auth.token = new URLSearchParams(window.location.search).get('token')!;
-        try {
-            teams.initialize(() => {
-                console.debug("teams initialized")
-                getUserId().then(id => {
-                    auth.id = id;
-                    resolve(auth);
-                }, reject)
-            })
-        } catch (error) {
-            console.error('initialize error', error)
-            reject(error)
-        }
-    })
+    auth.token = new URLSearchParams(window.location.search).get('token')!;
+    return getUserId().then(id => {
+        auth.id = id;
+        return auth
+    });
+    // return new Promise<typeof auth>((resolve, reject) => {
+       
+    //     try {
+    //         getUserId().then(id => {
+    //             auth.id = id;
+    //             resolve(auth);
+    //         }, reject)
+    //     } catch (error) {
+    //         console.error('initialize error', error)
+    //         reject(error)
+    //     }
+    // })
 }
 
 export function exit(result?: string) {
     teams.authentication.notifySuccess(result)
 }
 
-export function getUserId(): Promise<string> {
+function getUserId(): Promise<string> {
     return new Promise(resolve => teams.getContext((c) => {
         console.debug(c)
         resolve(c.userObjectId!)
