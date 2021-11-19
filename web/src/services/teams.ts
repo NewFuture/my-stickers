e2e1b2270ab2f9c80d13f333eafc2ecf61ddd0b2
NewFuture <New.Future@microsoft.com>
2021-11-19 19:38:48 +0800

    update teams sdk

--- web/src/services/teams.ts
+++ web/src/services/teams.ts
@@ -1,40 +1,25 @@
-import * as teams from "@microsoft/teams-js";
+import { app } from "@microsoft/teams-js";
 
 export const auth = {
     id: '',
     token: '',
 }
 
-teams.initialize(() => {console.debug("teams initialized")})
+
+app.initialize().then(() => { console.debug("teams initialized") });
 
 export function init() {
     auth.token = new URLSearchParams(window.location.search).get('token')!;
-    return getUserId().then(id => {
-        auth.id = id;
+    return app.initialize().then(() =>
+        app.getContext()
+    ).then((context) => {
+        console.debug(context);
+        auth.id = context.user?.id!;
         return auth
     });
-    // return new Promise<typeof auth>((resolve, reject) => {
-       
-    //     try {
-    //         getUserId().then(id => {
-    //             auth.id = id;
-    //             resolve(auth);
-    //         }, reject)
-    //     } catch (error) {
-    //         console.error('initialize error', error)
-    //         reject(error)
-    //     }
-    // })
 }
 
-export function exit(result?: string) {
-    teams.authentication.notifySuccess(result)
+export function exit() {
+    return app.notifySuccess();
+    
 }
-
-function getUserId(): Promise<string> {
-    return new Promise(resolve => teams.getContext((c) => {
-        console.debug(c)
-        resolve(c.userObjectId!)
-    }));
-}
-
