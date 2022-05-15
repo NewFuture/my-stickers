0221466f8b34429b3f1a89434dd5b16142afbd1d
NewFuture <New.Future@microsoft.com>
2022-05-15 19:09:43 +0800

    change auth method

--- server/src/teams/getConfigUrl.ts
+++ server/src/teams/getConfigUrl.ts
@@ -2,5 +2,5 @@ import { ENV } from "../config";
 import { generateToken } from "../services/token";
 
 export function getConfigUrl(id: string) {
-    return `https://${ENV.WEB_HOST}${ENV.WEB_CONFIG_PATH}?token=${generateToken(id)}`;
+    return `https://${ENV.WEB_HOST}${ENV.WEB_CONFIG_PATH}?id=${id}#${generateToken(id)}`;
 }
--- web/src/services/teams.ts
+++ web/src/services/teams.ts
@@ -9,14 +9,17 @@ export const auth = {
 app.initialize().then(() => { console.debug("teams initialized") });
 
 export function init() {
-    auth.token = new URLSearchParams(window.location.search).get('token')!;
-    return app.initialize().then(() =>
-        app.getContext()
-    ).then((context) => {
-        console.debug(context);
-        auth.id = context.user?.id!;
-        return auth
-    });
+    auth.id = new URLSearchParams(window.location.search).get('id')!;
+    auth.token = window.location.hash;
+    return Promise.resolve(auth);
+
+    // return app.initialize().then(() =>
+    //     app.getContext()
+    // ).then((context) => {
+    //     console.debug(context);
+    //     auth.id = context.user?.id!;
+    //     return auth
+    // });
 }
 
 export function exit() {
