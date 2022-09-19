456b66056789b8bdfb6fd406be350dfbd6492878
vinfol <37006451+vinfol@users.noreply.github.com>
2022-09-19 21:19:09 +0800

    init (#97)
    * init
    
    * reset dev port
    
    * reset proxy
    
    Co-authored-by: wenfengwang <wenfengwang@microsoft.com>

--- client-config-app/src/list/image-list.tsx
+++ client-config-app/src/list/image-list.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ImageItem from "./image-item";
 import { Sticker } from "../model/sticker";
 import { deleteSticker, editSticker } from "../services/stickers";
+import { UploadButton } from "./UploadButton";
 
 interface Props {
     items: Sticker[];
@@ -10,6 +11,7 @@ interface Props {
 const ImageList: React.FC<Props> = (props: Props) => {
     return (
         <div className="just-items-center">
+            <UploadButton />
             {props.items.map((item) => (
                 <ImageItem
                     key={item.id}
--- client-config-app/src/services/stickers.ts
+++ client-config-app/src/services/stickers.ts
@@ -1,93 +1,50 @@
 import { API } from "../lib/http";
 import { Sticker } from "../model/sticker";
-import { store } from "../lib/store";
-import { ActionType } from "../reducer";
 import { getUploadSAS, upload, SasInfo } from "./upload";
+import useSWR from "swr";
+import axios from "axios";
+import { auth } from "./teams";
 
-export async function getStickers(): Promise<void> {
-    store.dispatch({ type: ActionType.Status_Pending });
-    try {
-        const result = await API.get<{ values: Sticker[] }>("/me/stickers");
-        store.dispatch({ type: ActionType.Status_Ready });
-        store.dispatch({ type: ActionType.STICKER_fetch, payload: result.data.values });
-    } catch (error) {}
+const fetcher = (url:string) =>
+    axios
+    //   .get(`${process.env.REACT_APP_API_ROOT}${url}` || `/api/${url}`, { headers: { Authorization: `${auth.id} ${auth.token}` } })
+      .get(`${process.env.REACT_APP_API_ROOT}${url}` || `/api/${url}`, { headers: { Authorization: 'b614c4c4-2d74-4e56-bc0d-cc0d79b2715f 1663756411131.u8TlauIHpr2mdT9KyrEjZiJxtd5ftEN6TIxCtkVAC3c' } })
+      .then((res) => res.data);
+
+export function useStickersList(isTenant:boolean) {
+    const {data,error} = useSWR(isTenant ? "tenant/stickers" : "me/stickers",fetcher);
+    return {
+        stickers: data?.values,
+        isLoading: !error && !data,
+        isError: error,
+    }
 }
 
 async function uploadSticker(file: File, sas: SasInfo) {
     // store.dispatch
     const result = await upload(file, sas, (p) => {
-        store.dispatch({
-            type: ActionType.STICKER_uploading,
-            payload: {
-                id: sas.id,
-                progress: p.percent,
-            },
-        });
-    });
-    store.dispatch({
-        type: ActionType.STICKER_uploadSuccess,
-        payload: {
-            id: sas.id,
-            src: result.data.src,
-        },
+        // Todo
     });
 }
 
 export async function uploadStickers(files: File[]) {
-    // files = files.filter((f, i) => i < max);
     const stickers: Sticker[] = files.map((f, i) => ({
         id: `${i}.${Math.random()}`,
         src: URL.createObjectURL(f),
         name: f.name.replace(/\..*$/, ""),
     }));
-    store.dispatch({
-        type: ActionType.STICKER_preupload,
-        payload: stickers,
-    });
 
     const sasInfos = await getUploadSAS({
         exts: files.map((f) => f.name.split(".").pop()!),
-    });
-    store.dispatch({
-        type: ActionType.STICKER_upload,
-        payload: sasInfos.map((sas, i) => ({
-            id: stickers[i].id,
-            new_id: sas.id,
-        })),
     });
 
     sasInfos.forEach((sas, i) => uploadSticker(files[i], sas));
 }
 
 export async function deleteSticker(id: string) {
-    store.dispatch({
-        type: ActionType.STICKER_delete,
-        payload: {
-            id,
-        },
-    });
     await API.delete(`/stickers/${id}`);
-    store.dispatch({
-        type: ActionType.STICKER_deleteSuccess,
-        payload: {
-            id,
-        },
-    });
 }
 
 export async function editSticker(id: string, name: string) {
-    store.dispatch({
-        type: ActionType.STICKER_edit,
-        payload: {
-            id,
-            name,
-        },
-    });
     await API.patch(`/stickers/${id}`, { name });
-    store.dispatch({
-        type: ActionType.STICKER_editSuccess,
-        payload: {
-            id,
-        },
-    });
 }
