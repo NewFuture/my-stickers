51b0408a11dbdc5a16433e07ad127f44930b3cf5
Howar wu <zhongw@microsoft.com>
2022-09-20 11:48:59 +0800

    add ut

---
+++ StickersTests/Service/BlobServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stickers.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stickers.Service.Tests
+{
+    [TestClass()]
+    public class BlobServiceTests
+    {
+        [TestMethod()]
+        public void getSasTokenTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
