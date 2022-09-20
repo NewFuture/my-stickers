f5ecc002503841309966ff1a9f6436a2111fa219
Howar wu <zhongw@microsoft.com>
2022-09-20 12:51:15 +0800

    update blob service

--- StickersTests/Service/BlobServiceTests.cs
+++ StickersTests/Service/BlobServiceTests.cs
@@ -11,10 +11,18 @@ namespace Stickers.Service.Tests
     [TestClass()]
     public class BlobServiceTests
     {
+        private string connectionString = "DefaultEndpointsProtocol=https;AccountName=stickerstestblob;AccountKey=iHF/+qta9aawXNS+2/d1+kiWusozauf059iEB6UhIeg+vD5LIRktA/KebxFZWDAVNvNeeJnAp88w+AStvquR3g==;EndpointSuffix=core.windows.net";
         [TestMethod()]
         public void getSasTokenTest()
         {
-            Assert.Fail();
+            BlobService blob = new BlobService(connectionString);
+            var uri =blob.getSasToken("testuser", "jpg").ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void commitTest()
+        {
+            BlobService blob = new BlobService(connectionString);
+            var uri = blob.commitBlocks("testuser","","jpg","image/jpg").ConfigureAwait(false).GetAwaiter().GetResult();
         }
     }
 }
