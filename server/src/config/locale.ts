de4d6ae1edcd1cdbf03669ff66beae039a1c629b
New Future <NewFuture@users.noreply.github.com>
2022-09-24 10:53:44 +0000

    remove node server

--- server/src/config/locale.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable-next-line:variable-name
-export const enum Locale {
-    pong = "pong",
-    config_action_title = "config_action_title",
-    collect_save_success = "collect_save_success",
-    collect_save_fail = "collect_save_fail",
-    collect_save_no_images_found = "collect_save_no_images_found",
-    initial_run_upload_stickers = "initial_run_upload_stickers",
-}
