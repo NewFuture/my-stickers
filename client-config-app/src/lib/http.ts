14acabcd452e3271ae21668ea837db4fbf6e133e
New Future (Weilai LIU) <New.Future@microsoft.com>
2022-09-24 15:26:55 +0800

    fix axios

--- client-config-app/src/lib/http.ts
+++ client-config-app/src/lib/http.ts
@@ -3,15 +3,16 @@ import { BASE_URL } from "./env";
 
 const SessionKey = window.location.hash?.substring(1);
 const USER_SEESION_HEADER = "Session-Key";
+
 export const API = axios.create({
     baseURL: BASE_URL,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
     },
-    // auth,
-    transformRequest: (data, headers) => {
-        headers[USER_SEESION_HEADER] = SessionKey;
-        return data;
-    },
+});
+
+API.interceptors.request.use((c) => {
+    c.headers[USER_SEESION_HEADER] = SessionKey;
+    return c;
 });
