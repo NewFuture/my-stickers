dd0013c03d522aff82a9fbf96d5a71c6e037be60
wenfengwang <wenfengwang@microsoft.com>
2022-09-25 18:23:05 +0800

    yarn eslint

--- client-config-app/src/components/WelcomePage/WelcomePage.tsx
+++ client-config-app/src/components/WelcomePage/WelcomePage.tsx
@@ -7,31 +7,31 @@ import { getContext } from "../../services/teams";
 import LoginPic from "../../assets/LoginPagePic.png";
 
 interface WelcomePageProps {
-  onLogin: (token: string) => void;
+    onLogin: (token: string) => void;
 }
 
 export const WelcomePage: React.FC<WelcomePageProps> = (props) => {
-  const { t } = useTranslation();
-  const styles = useWelcomePageStyles();
-  const loginDescription = t(TransKeys.loginDescription);
-  const action = t(TransKeys.login);
-  const [link, setLink] = useState<string>();
-  useEffect(() => {
-    getContext().then((c) => {
-      const tenantId = c.user?.tenant?.id;
-      const link = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${process.env.REACT_APP_AAD_ID}&response_type=code&redirect_uri=${window.location.origin}&scope=openid`;
-      setLink(link);
-    });
-  }, []);
-  return (
-    <div className={styles.root}>
-      <Image className={styles.img} src={LoginPic} />
-      <div className={styles.description}>
-        <Text>{loginDescription}</Text>
-      </div>
-      <Button appearance="primary" as="a" disabled={!link} href={link} target="_blank">
-        {action}
-      </Button>
-    </div>
-  );
+    const { t } = useTranslation();
+    const styles = useWelcomePageStyles();
+    const loginDescription = t(TransKeys.loginDescription);
+    const action = t(TransKeys.login);
+    const [link, setLink] = useState<string>();
+    useEffect(() => {
+        getContext().then((c) => {
+            const tenantId = c.user?.tenant?.id;
+            const link = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${process.env.REACT_APP_AAD_ID}&response_type=code&redirect_uri=${window.location.origin}&scope=openid`;
+            setLink(link);
+        });
+    }, []);
+    return (
+        <div className={styles.root}>
+            <Image className={styles.img} src={LoginPic} />
+            <div className={styles.description}>
+                <Text>{loginDescription}</Text>
+            </div>
+            <Button appearance="primary" as="a" disabled={!link} href={link} target="_blank">
+                {action}
+            </Button>
+        </div>
+    );
 };
