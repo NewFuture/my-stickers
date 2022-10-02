85ed7f757084bddf81721ea8c4704d0e655b8f53
NewFuture <New.Future@microsoft.com>
2022-10-02 14:06:18 +0800

    only revalidateOnFocus for tenants

--- client-config-app/src/hooks/useStickersList.ts
+++ client-config-app/src/hooks/useStickersList.ts
@@ -15,12 +15,10 @@ export function useStickersList(isTenant: boolean): StickerListResult {
     const url = isTenant ? `/admin/stickers` : "/me/stickers";
     const result: StickerListResult = useSWR(url, fetcher, {
         refreshInterval: 5 * 60 * 1000, // 5 mins
-        focusThrottleInterval: 10 * 1000, //10s
+        revalidateOnFocus: isTenant, // only for tenant
         revalidateIfStale: false, // automatically revalidate even if there is stale data
-        errorRetryCount: 5,
-        shouldRetryOnError: (err) => {
-            return !isUnauthorizedError(err);
-        },
+        errorRetryCount: 4,
+        shouldRetryOnError: (err) => !isUnauthorizedError(err),
     });
     result.isLoading = !result.data && !result.error;
     return result;
