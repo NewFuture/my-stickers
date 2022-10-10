165994ee532f86a4df24cd5f93227c57057ec9c5
New Future <NewFuture@users.noreply.github.com>
2022-10-10 15:50:00 +0800

    Bot telemetry (#227)
    * bot telemetery
    
    * rename
    
    * lint

---
+++ server/Entities/OfficialSticker.cs
@@ -0,0 +1,13 @@
+﻿namespace Stickers.Entities;
+
+public class OfficialSticker
+{
+    public string? img { get; set; }
+    public string? name { get; set; }
+    public string? type { get; set; }
+
+    public List<string>? keywords { get; set; }
+    public Dictionary<string, string>? localName { get; set; }
+    public string? url { get; set; }
+    public string? collection { get; set; }
+}
--- server/Service/SearchService.cs
+++ server/Service/SearchService.cs
@@ -1,52 +1,46 @@
-namespace Stickers.Service
+namespace Stickers.Service;
+using Stickers.Entities;
+
+public class SearchService
 {
-    using Stickers.Entities;
-    using Stickers.Search;
+    private OfficialStickersService officialStickersService;
+    private StickerService stickerService;
 
-    public class SearchService
+    public SearchService(
+        OfficialStickersService officialStickersSearchHandler,
+        StickerService stickerService
+    )
     {
-        private OfficialStickersSearchHandler officialStickersSearch;
-        private StickerService stickerService;
+        this.stickerService = stickerService;
+        this.officialStickersService = officialStickersSearchHandler;
+    }
 
-        public SearchService(
-            OfficialStickersSearchHandler officialStickersSearchHandler,
-            StickerService stickerService
-        )
+    public async Task<List<Sticker>> SearchUserStickers(Guid userId, string? keyword)
+    {
+        var userStickers = await stickerService.getUserStickers(userId);
+        if (String.IsNullOrWhiteSpace(keyword))
         {
-            this.stickerService = stickerService;
-            this.officialStickersSearch = officialStickersSearchHandler;
+            return userStickers;
         }
+        //  Regex regex = new Regex(search.Trim().Replace("\\s+", ".*"));
+        // imageEntities = imageEntities.Where(i => !string.IsNullOrEmpty(i.name) && regex.IsMatch(i.name)).ToList();
+        return userStickers.FindAll(s => s.name?.ToLower().Contains(keyword) ?? false);
+    }
 
-        public async Task<List<Sticker>> SearchUserStickers(Guid userId, string? keyword)
+    public async Task<List<Sticker>> SearchTenantStickers(Guid tenantId, string? keyword)
+    {
+        var stickers = await stickerService.getTenantStickers(tenantId);
+        if (String.IsNullOrWhiteSpace(keyword))
         {
-            var userStickers = await stickerService.getUserStickers(userId);
-            if (String.IsNullOrWhiteSpace(keyword))
-            {
-                return userStickers;
-            }
-            //  Regex regex = new Regex(search.Trim().Replace("\\s+", ".*"));
-            // imageEntities = imageEntities.Where(i => !string.IsNullOrEmpty(i.name) && regex.IsMatch(i.name)).ToList();
-            return userStickers.FindAll(s => s.name?.ToLower().Contains(keyword) ?? false);
+            return stickers;
         }
+        //  Regex regex = new Regex(search.Trim().Replace("\\s+", ".*"));
+        // imageEntities = imageEntities.Where(i => !string.IsNullOrEmpty(i.name) && regex.IsMatch(i.name)).ToList();
+        return stickers.FindAll(s => s.name?.ToLower().Contains(keyword) ?? false);
+    }
 
-        public async Task<List<Sticker>> SearchTenantStickers(Guid tenantId, string? keyword)
-        {
-            var stickers = await stickerService.getTenantStickers(tenantId);
-            if (String.IsNullOrWhiteSpace(keyword))
-            {
-                return stickers;
-            }
-            //  Regex regex = new Regex(search.Trim().Replace("\\s+", ".*"));
-            // imageEntities = imageEntities.Where(i => !string.IsNullOrEmpty(i.name) && regex.IsMatch(i.name)).ToList();
-            return stickers.FindAll(s => s.name?.ToLower().Contains(keyword) ?? false);
-        }
-
-        public async Task<List<Models.OfficialSticker>> SearchOfficialStickers(
-            string? keyword,
-            int top = 30
-        )
-        {
-            return await this.officialStickersSearch.Search(keyword!);
-        }
+    public async Task<List<OfficialSticker>> SearchOfficialStickers(string? keyword, int top = 30)
+    {
+        return await this.officialStickersService.Search(keyword!);
     }
 }
