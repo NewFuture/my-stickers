c49ef1b85cd4604eb0d48e01b28fb9ee6de1e185
New Future (Weilai LIU) <New.Future@microsoft.com>
2022-10-19 21:39:19 +0800

    add snapshot testing

--- website/src/App.test.tsx
+++ website/src/App.test.tsx
@@ -1,19 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
-if (document) {
-    document.createRange = () =>
-        ({
-            setStart: () => {},
-            setEnd: () => {},
-            commonAncestorContainer: {
-                nodeName: "BODY",
-                ownerDocument: document,
-            },
-        } as any as Range);
-}
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+import { create } from "react-test-renderer";
+test("Website Snapshot", () => {
+    const tree = create(<App />); // 渲染
+    expect(tree).toMatchSnapshot();
 });
