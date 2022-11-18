4211de35f281387b4e5a74a9cce1ec1ad2a40143
New Future (Weilai LIU) <New.Future@microsoft.com>
2022-11-18 13:59:18 +0800

    SESSION_HEADER_KEY

--- server/Utils/IHeaderDictionaryExtension.cs
+++ server/Utils/IHeaderDictionaryExtension.cs
@@ -2,15 +2,20 @@ namespace Stickers.Utils;
 
 public static class IHeaderDictionaryExtension
 {
-    public const string SESSION_HEADER_KEY = "Session-Key";
+    // user Content-Language to make the simple_requests avoid Preflight request
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#simple_requests
+    public const string SESSION_HEADER_KEY = "Content-Language";
+
+    public const string FALLBACK_SESSION_HEADER_KEY = "Session-Key";
 
     public static bool TryGetSessionId(this IHeaderDictionary headers, out Guid value)
     {
-        if (headers.TryGetValue(SESSION_HEADER_KEY, out var sessionKey))
+        if (headers.TryGetValue(SESSION_HEADER_KEY, out var sessionKey) && Guid.TryParse(sessionKey, out value))
         {
-            return Guid.TryParse(sessionKey, out value);
+            return true;
         }
 
-        return Guid.TryParse(headers.AcceptLanguage.FirstOrDefault(), out value);
+        value = default;
+        return headers.TryGetValue(FALLBACK_SESSION_HEADER_KEY, out sessionKey) && Guid.TryParse(sessionKey, out value);
     }
 }
