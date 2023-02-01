d856d7e4a342c555dfbc8ed6ec953c65334eb9fc
New Future <NewFuture@users.noreply.github.com>
2023-02-01 09:48:54 +0000

    fix type

--- client-config-app/src/hooks/useStickersList.ts
+++ client-config-app/src/hooks/useStickersList.ts
@@ -4,13 +4,9 @@ import type { Sticker } from "../model/sticker";
 
 const fetcher = (url: string) => API.get<{ values: Sticker[] }>(url).then((res) => res.data?.values);
 
-interface StickerListResult extends SWRResponse<Sticker[], any> {
-    isLoading?: boolean;
-}
-
-export function useStickersList(isTenant: boolean): StickerListResult {
+export function useStickersList(isTenant: boolean) {
     const url = isTenant ? tenantList : myList;
-    const result: StickerListResult = useSWR(url, fetcher, {
+    const result: = useSWR(url, fetcher, {
         refreshInterval: (isTenant ? 5 : 10) * 60 * 1000, // 10 mins
         revalidateOnFocus: isTenant, // only for tenant
     });
